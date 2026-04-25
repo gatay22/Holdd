e7c58c653c080e33dc8b1cc1ea188a3e1ef06b15
gatay22 <tangguljatiroto@gmail.com>
2026-04-25 21:22:37 +0700

    Create AstralMod.java

---
+++ net/astral/AstralMod.java
@@ -0,0 +1,10 @@
+package net.astral;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AstralMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // Kosongkan saja, ini hanya syarat agar Fabric kenal mod lo
+    }
+}
